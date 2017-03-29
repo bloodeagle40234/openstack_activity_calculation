27ad7d4492a193d211711d9db91a8e07da279b43
Kazuhiro MIYAHARA <miyahara.kazuhiro@lab.ntt.co.jp>
2017-03-29 10:36:41 +0900

    Dump meeting voices to html (prototype)

--- meetings/meeting_log_collector.py
+++ meetings/meeting_log_collector.py
@@ -1,7 +1,12 @@
-from html.parser import HTMLParser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from HTMLParser import HTMLParser
 import requests
 from datetime import datetime
 import os
+import itertools
+import sys
 
 
 LOG_HOST = 'http://eavesdrop.openstack.org'
@@ -34,15 +39,19 @@ class NormalLogParser(object):
     def base_url(self):
         return os.path.join(LOG_HOST, self.channel)
 
-    def latter_than(self, base_datetime, log_name):
-        # log name should be like swift.2016-09-07-21.00.log.txt
-        if log_name == 'latest.log.html':
-            return False
+    def log_date_of(self, log_name):
         date_str = log_name.split('.')[1]
         parsed = self.split_log_date(date_str)
         log_date = datetime(
             year=int(parsed[0]), month=int(parsed[1]), day=int(parsed[2]))
-        return log_date > base_datetime
+        return log_date
+
+    def in_range_of(self, start_datetime, end_datetime, log_name):
+        # log name should be like swift.2016-09-07-21.00.log.txt
+        if log_name == 'latest.log.html':
+            return False
+        log_date = self.log_date_of(log_name)
+        return (start_datetime <= log_date) and (log_date <= end_datetime)
 
     def split_log_date(self, date_str):
         return date_str.split('-', 3)
@@ -52,6 +61,39 @@ class NormalLogParser(object):
 
     def wrap_name(self, name):
         return self.name_wrap % name
+
+    def count_voices(self, contributor_names, log_names):
+        count = {}
+
+        sys.stderr.write('PROGRESS...\n')
+        for i in range(len(log_names)):
+            sys.stderr.write('_')
+        sys.stderr.write('\n')
+
+        for i, log_name in enumerate(log_names):
+            sys.stderr.write('>')
+            alog = os.path.join(self.base_url, log_name)
+            log_date = self.log_date_of(log_name)
+            resp = requests.get(alog)
+            for (line, contributor) in itertools.product(
+                    resp.content.split('\n'), contributor_names):
+                wrapped_name = self.wrap_name(contributor)
+                if wrapped_name in line:
+                    # ignoring just join/quit message for the channel
+                    for ignore in JOIN_QUIT:
+                        if ignore in line:
+                            break
+                    else:
+                        monthly_count = count.setdefault(log_date.month, {})
+                        daily_count =\
+                            monthly_count.setdefault(log_date.day, {})
+                        count_of_the_contributor =\
+                            daily_count.setdefault(contributor, 0)
+                        daily_count[contributor] =\
+                            count_of_the_contributor + 1
+        sys.stderr.write('\n')
+
+        return count
 
 
 class MeetingLogParser(NormalLogParser):
@@ -75,38 +117,77 @@ class MeetingLogParser(NormalLogParser):
         return os.path.join(LOG_HOST, self.channel, self.year)
 
 
-def collect_my_voice(whoami, logs):
-    voices = []
-    for alog in logs:
-        resp = requests.get(alog)
-        for line in resp.content.split('\n'):
-            if whoami in line:
-                # ignoring just join/quit message for the channel
-                for ignore in JOIN_QUIT:
-                    if ignore in line:
-                        break
-                else:
-                    voices.append(line)
-    return voices
-
 
 if __name__ == '__main__':
-    year = 2017
-    # choose MeetingLogParser or NormalLogParser
-    log_parser = MeetingLogParser(year)
-    # log_parser = NormalLogParser()
-    base_datetime = datetime(year=year, month=3, day=17)
+    year = 2016
+    start_datetime = datetime(year=year, month=1, day=1)
+    end_datetime = datetime(year=year, month=12, day=31)
+    contributor_names = [
+        'kota_', 'm_kazuhiro', 'dmorita', 'hosanai', 'notmyname', 'clayg',
+        'acoles', 'tdasilva']
+    collect_meeting_log = False
+
+    log_parser =\
+        MeetingLogParser(year) if collect_meeting_log else NormalLogParser()
     resp = requests.get(os.path.join(log_parser.base_url))
     parser = LinkParser()
     parser.feed(resp.content)
-    logs = [os.path.join(log_parser.base_url, item)
-            for item in parser.href_attrs
-            if log_parser.ext_check(item) and
-            log_parser.latter_than(base_datetime, item)]
-    voices = collect_my_voice(log_parser.wrap_name('kota_'), logs)
-    # join is the count of logs (er - just a # of collected logs,
-    # it may not join count actually)
-    print('# of join: %s' % len(logs))
-    # voice is the count of lines your name appeared in the line.
-    # that can contain the responses for you from other people.
-    print('# of voices: %s' % len(voices))
+
+    log_names = [log_name for log_name in parser.href_attrs
+                 if log_parser.ext_check(log_name) and
+                 log_parser.in_range_of(start_datetime, end_datetime, log_name)]
+    count = log_parser.count_voices(contributor_names, log_names)
+
+
+    print '<html>'
+    print '<body>'
+
+    # all sum
+    print str(year) + "年 合計"
+    print '<table border="1">'
+    print '<tr>' + ''.join(['<th>' + _ + '</th>'
+        for _ in ['all'] + contributor_names]) + '</tr>'
+    all_sum = {}
+    for month in count:
+        for day in count[month]:
+            for contributor in contributor_names:
+                count_sum = all_sum.setdefault(contributor, 0)
+                count_sum += count[month][day].setdefault(contributor, 0)
+                all_sum[contributor] = count_sum
+    print '<tr>' + ''.join(['<td>' + _ + '</td>'
+        for _ in ['all'] + [str(all_sum.setdefault(contributor, 0))
+        for contributor in contributor_names]]) + '</tr>'
+    print '</table>'
+
+    # monthly sum
+    print str(year) + "年 月ごと"
+    print '<table border="1">'
+    print '<tr>' + ''.join(['<th>' + _ + '</th>'
+        for _ in ['month'] + contributor_names]) + '</tr>'
+    for month in count:
+        monthly_sum = {}
+        for day in count[month]:
+            for contributor in contributor_names:
+                count_sum = monthly_sum.setdefault(contributor, 0)
+                count_sum += count[month][day].setdefault(contributor, 0)
+                monthly_sum[contributor] = count_sum
+        print '<tr>' + ''.join(['<td>' + _ + '</td>'
+            for _ in [str(month)] + [str(monthly_sum.setdefault(contributor, 0))
+            for contributor in contributor_names]]) + '</tr>'
+    print '</table>'
+
+    # daily count
+    print str(year) + "年 日ごと"
+    print '<table border="1">'
+    print '<tr>' + ''.join(['<th>' + _ + '</th>'
+        for _ in ['day'] + contributor_names]) + '</tr>'
+    for month in count:
+        for day in count[month]:
+            print '<tr>' + ''.join(['<td>' + _ + '</td>'
+                for _ in ['/'.join([str(month), str(day)])] +
+                [str(count[month][day].setdefault(contributor, 0))
+                for contributor in contributor_names]]) + '</tr>'
+    print '</table>'
+
+    print '</html>'
+    print '</body>'
