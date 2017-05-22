9053ea04a45e3983b596b7b1b5b33513aa56c3d9
Kota Tsuyuzaki <tsuyuzaki.kota@lab.ntt.co.jp>
2017-05-22 01:09:09 -0700

    Refactor stackalytics/html_dumper
    1. parse CommitCollector to CommitCollector and LoCCollector
    2. refactor redundant dump methods
    3. avoid hard coded release name in modules
    4. fix some pep8 erros
    
    *NOT YET*
    1. refactor also ReviewCollector
    2. parge Collector to Summarizer and Collector

--- stackalytics/html_dumper.py
+++ stackalytics/html_dumper.py
@@ -14,36 +14,23 @@ releases = [
     'kilo', 'liberty', 'mitaka', 'newton', 'ocata',
     'pike']
 
-class CommitCollector(object):
-    user_id_list = [
-        'tsuyuzaki-kota', 'miyahara-kazuhiro', 'notmyname', 'clay-gerrard',
-        'alistair-coles', 'cschwede', 'thiagodasilva']
 
-    #user_id_list = [
-    #    'tsuyuzaki-kota', 'miyahara-kazuhiro', 'marek-kaleta', 'charles0126',
-    #    'tuanla', 'romain-ledisez', 'kellerbr', 'mahati-chamarthy',
-    #    'confisurya', 'diazjf', 'junboli', 'jsbryant', 'v-iswarya',
-    #    'wanghongtaozz', 'vancsics', 'guojianhua', 'mohit-motiani', 'dolph',
-    #    'janonymous', 'jeremy.zhang', 'kevin-zhao', 'sacpatil', 'miethe',
-    #    'harinipradeep27', 'jrichli', 'rahulunair', 'travis-mcpeak',
-    #    'clay-gerrard', 'chen-xing', 'joel-wright-k', 'alistair-coles',
-    #    'o-tony', 'tnovacik-6', '1-tim-z', 'lidong007', 'thurloat',
-    #    'paul-dardeau', 'jaegerandi', 'yuhui', 'dims-v', 'indicoliteplus',
-    #    'nandini-tata', 'tuhv', 'pavel.kvasnicka@firma.seznam.cz', 'jaosorior',
-    #    'ericwb', 'yangjd', 'onovy', 'falk-reimann', 'alexisl', 'alecuyer',
-    #    'arnaud-morin', 'briancline', 'dunght', 'treinish', 'torgomatic',
-    #    'notmyname', 'karenc', 'richard-hawkins', 'cschwede', 'peterlisak',
-    #    'zhanghj', 'trananhkma', 'matt-0', 'gabor.antal', 'sharat-sharma',
-    #    'kaitlin-farr', 'yin-zheng', 'glonlas', 'thiagodasilva', 'caowei-e',
-    #    'aerwin3', 'hongkai', 'zaitcev', 'hoangcx']
-
+class BaseCollector(object):
     page_size = 100
-    base_filter_params = {
-        'metric': 'commits', 'page_size': page_size, 'start_record': 0}
 
-    def collect_activities_per_user(self, user_id, release):
+    def collect_activities(self, user_id, release, params=None):
+        """
+        collect user activiety for the specific release
+
+        :param user_id: launchpad user id to collect
+        :param release: release name of openstack to collect, "all" can be
+            allowed to collect all activities
+        :param params: parameter dict
+        :return activities: a list of activity dict
+        """
+        params = params or {}
         filter_params = {'user_id': user_id, 'release': release}
-        filter_params.update(self.base_filter_params)
+        filter_params.update(params)
         activities = []
         while True:
             resp = requests.get(
@@ -55,13 +42,17 @@ class CommitCollector(object):
                 break
         return activities
 
-    def commits(self):
+    def collect_commit_info(self, releases=['all']):
+        """
+        :param releases: which release caller wants to collect
+        :return: a dict which maps release -> user_id -> commit info
+        """
         commits = {}
         for release in releases:
             commits_per_release = {}
             for user_id in self.user_id_list:
-                activities_per_user_and_release =\
-                    self.collect_activities_per_user(user_id, release)
+                activities_per_user_and_release = self.collect_activities(
+                    user_id, release, self.filter_params)
                 commits_per_user_and_release = []
                 for activity in activities_per_user_and_release:
                     commits_per_user_and_release.append({
@@ -74,74 +65,102 @@ class CommitCollector(object):
             commits[release] = commits_per_release
         return commits
 
-    def table_headers(self):
-        return ['release'] + self.user_id_list
+    def dump_result(self, result, dump_format='html'):
+        """
+        :param result: a result dict that maps reslease -> user_id -> value
+        :param dump_format:
+        """
+        if dump_format not in ('html', 'csv'):
+            raise ValueError('dump_format is supported only for html or csv')
 
-    def table_bodies(self, result, release):
-        return [release] + [str(result[release][user_id])
-            for user_id in self.user_id_list]
+        # release_to_activity_list is a dict to map a release to user
+        # activities (perhaps, we don't have to keep the release for the key
+        # because each row keep the release name in the 1st column)
+        release_to_activity_list = dict()
 
-    def dump_result_in_csv(self, result, delimiter=','):
-        print delimiter.join(self.table_headers())
-        for release in releases:
-            print delimiter.join(self.table_bodies(result, release))
+        for release, result_info in result.items():
+            row_list = [release]
+            for user_id in self.user_id_list:
+                # N.B. the result set may be already filtered when
+                # collecting the result but for safety, use .get and set
+                # 0 if the user id not found in the dict)
+                row_list.append(str(result_info.get(user_id, 0)))
+            release_to_activity_list[release] = row_list
 
-    def dump_result_in_html(self, result):
-        print '<html>'
-        print '<body>'
-        print '<table border="1">'
+        if dump_format == 'csv':
+            # dump as csv
+            print(','.join(self.titles()))
+            for _, row_list in release_to_activity_list.items():
+                print(','.join(row_list))
+        else:
+            # dump as html
+            title_row = '<tr>%s</tr>' % ''.join(
+                ['<th>%s</th>' % title for title in self.titles()])
+            body_rows = ''
+            for _, row_list in release_to_activity_list.items():
+                body_rows += '<tr>%s</tr>' % ''.join(
+                    ['<td>%s</td>' % item for item in row_list])
 
-        print '<tr>'
-        print ''.join(['<th>' + _ + '</th>' for _ in self.table_headers()])
-        print '</tr>'
+            print('<html><body><table border="1">%s%s</table></body></html>'
+                  % (title_row, body_rows))
 
-        for release in releases:
-            print '<tr>'
-            print ''.join(['<td>' + _ + '</td>'
-                for _ in self.table_bodies(result, release)])
-            print '</tr>'
+    def titles(self):
+        """
+        :return: a title row list which is like as
+            ['release', 'user-id1', 'user-id2'....]
+        """
+        title_row = ['release']
+        title_row.extend(self.user_id_list)
+        return title_row
 
-        print '</table>'
-        print '</body>'
-        print '</html>'
 
-    def lines_of_code_sum(self):
-        commits = self.commits()
-        lines_of_code_sum = {}
-        for release in commits:
-            lines_of_code_sum_per_release = {}
-            for user_id in commits[release]:
-                lines_of_code_sum_per_user_and_release = 0
-                for commit in commits[release][user_id]:
-                    lines_of_code_sum_per_user_and_release +=\
-                        commit['lines_of_code']
-                lines_of_code_sum_per_release[user_id] =\
-                    lines_of_code_sum_per_user_and_release
-            lines_of_code_sum[release] = lines_of_code_sum_per_release
-        return lines_of_code_sum
+class CommitCollector(BaseCollector):
+    user_id_list = [
+        'tsuyuzaki-kota', 'miyahara-kazuhiro', 'notmyname', 'clay-gerrard',
+        'alistair-coles', 'cschwede', 'thiagodasilva']
 
-    def dump_lines_of_code_sum_in_csv(self):
-        self.dump_result_in_csv(self.lines_of_code_sum())
+    def __init__(self):
+        self.filter_params = {
+            'metric': 'commits',
+            'page_size': self.page_size,
+            'start_record': 0}
 
-    def dump_lines_of_code_sum_in_html(self):
-        self.dump_result_in_html(self.lines_of_code_sum())
-
-    def commit_counts(self):
-        commits = self.commits()
+    def commit_count(self, commit_info_dict):
+        """
+        summarize
+        :param commit_info_dict: a raw infor dict that collect_commit_info
+            returns
+        :return: summary info that maps release -> user_id -> value
+        """
         commit_counts = {}
-        for release in commits:
+        for release, commit_dict in commit_info_dict.items():
             commit_counts_per_release = {}
-            for user_id in commits[release]:
-                commit_counts_per_release[user_id] =\
-                    len(commits[release][user_id])
+            for user_id, commit_list in commit_dict.items():
+                commit_counts_per_release[user_id] = len(commit_list)
             commit_counts[release] = commit_counts_per_release
         return commit_counts
 
-    def dump_commit_counts_in_csv(self):
-        self.dump_result_in_csv(self.commit_counts())
 
-    def dump_commit_counts_in_html(self):
-        self.dump_result_in_html(self.commit_counts())
+class LoCCollector(BaseCollector):
+    user_id_list = [
+        'tsuyuzaki-kota', 'miyahara-kazuhiro', 'notmyname', 'clay-gerrard',
+        'alistair-coles', 'cschwede', 'thiagodasilva']
+
+    def lines_of_code(self, commit_info_dict):
+        """
+        summarize
+        :param commit_info_dict: a raw infor dict that collect_commit_info
+            returns
+        :return: summary info that maps release -> user_id -> value
+        """
+        lines_of_code = {}
+        for release, commit_dict in commit_info_dict.items():
+            user_to_loc = {}
+            for user_id, commit_list in commit_dict.items():
+                user_to_loc[user_id] = sum(
+                    commit['lines_of_code'] for commit in commit_list)
+            lines_of_code[release] = user_to_loc
+        return lines_of_code
 
 
 class ReviewCollector(object):
@@ -165,40 +184,6 @@ class ReviewCollector(object):
                 review_counts_per_release[stat['name']] = stat['metric']
             review_counts[release] = review_counts_per_release
         return review_counts
-
-    def table_headers(self):
-        return ['release'] + self.target_names
-
-    def table_bodies(self, stats, release):
-        return [release] + [str(stats[release].setdefault(target_name, 0))
-            for target_name in self.target_names]
-
-    def dump_review_counts_in_csv(self, delimiter=','):
-        stats = self.review_counts()
-        print delimiter.join(self.table_headers())
-        for release in releases:
-            print delimiter.join(self.table_bodies(stats, release))
-
-    def dump_review_counts_in_html(self):
-        stats = self.review_counts()
-
-        print '<html>'
-        print '<body>'
-        print '<table border="1">'
-
-        print '<tr>'
-        print ''.join(['<th>' + _ + '</th>' for _ in self.table_headers()])
-        print '</tr>'
-
-        for release in releases:
-            print '<tr>'
-            print ''.join(['<td>' + _ + '</td>'
-                for _ in self.table_bodies(stats, release)])
-            print '</tr>'
-
-        print '</table>'
-        print '</body>'
-        print '</html>'
 
 
 class EngineerReviewCollector(ReviewCollector):
@@ -230,9 +215,17 @@ class CompanyReviewCollector(ReviewCollector):
 
 
 if __name__ == '__main__':
-    #review_counts_collector = CompanyReviewCollector()
-    #review_counts_collector.dump_review_counts_in_html()
+    # review_counts_collector = CompanyReviewCollector()
+    # review_counts_collector.dump_review_counts_in_html()
 
     commit_collector = CommitCollector()
-    commit_collector.dump_lines_of_code_sum_in_html()
+    info = commit_collector.collect_commit_info(['pike'])
 
+    # commit
+    # result = commit_collector.commit_count(info)
+    # commit_collector.dump_result(result)
+
+    # loc
+    loc_collector = LoCCollector()
+    result = loc_collector.lines_of_code(info)
+    loc_collector.dump_result(result)
