dffed6f1dd3e81589b37a1661dce0a95bd971be4
Kota Tsuyuzaki <tsuyuzaki.kota@lab.ntt.co.jp>
2017-05-22 03:06:23 -0700

    Fix ReviewCollecter to take similar syntax with CommitCollector
    And make dump_result as module function which can be used with
    ReviewCollector result too.

--- stackalytics/html_dumper.py
+++ stackalytics/html_dumper.py
@@ -2,8 +2,8 @@ import requests
 import os.path
 import json
 
-base_url = 'http://stackalytics.com/api/'
-version = '1.0'
+BASE_URL = 'http://stackalytics.com/api/'
+VERSION = '1.0'
 api = 'stats'
 
 releases = [
@@ -26,7 +26,7 @@ class CommitCollector(object):
             'page_size': self.page_size,
             'start_record': 0}
         self.user_id_list = user_id_list or self.user_id_list
-        self.url = os.path.join(base_url, version, 'activity')
+        self.url = os.path.join(BASE_URL, VERSION, 'activity')
 
     def collect_activities(self, user_id, release, params=None):
         """
@@ -51,11 +51,12 @@ class CommitCollector(object):
                 break
         return activities
 
-    def collect_commit_info(self, releases=['all']):
+    def collect_commit_info(self, releases=None):
         """
         :param releases: which release caller wants to collect
         :return: a dict which maps release -> user_id -> commit info
         """
+        releases = releases or ['all']
         commits = {}
         for release in releases:
             commits_per_release = {}
@@ -74,53 +75,58 @@ class CommitCollector(object):
             commits[release] = commits_per_release
         return commits
 
-    def dump_result(self, result, dump_format='html'):
-        """
-        :param result: a result dict that maps reslease -> user_id -> value
-        :param dump_format:
-        """
-        if dump_format not in ('html', 'csv'):
-            raise ValueError('dump_format is supported only for html or csv')
-
-        # release_to_activity_list is a dict to map a release to user
-        # activities (perhaps, we don't have to keep the release for the key
-        # because each row keep the release name in the 1st column)
-        release_to_activity_list = dict()
-
-        for release, result_info in result.items():
-            row_list = [release]
-            for user_id in self.user_id_list:
-                # N.B. the result set may be already filtered when
-                # collecting the result but for safety, use .get and set
-                # 0 if the user id not found in the dict)
-                row_list.append(str(result_info.get(user_id, 0)))
-            release_to_activity_list[release] = row_list
-
-        if dump_format == 'csv':
-            # dump as csv
-            print(','.join(self.titles()))
-            for _, row_list in release_to_activity_list.items():
-                print(','.join(row_list))
-        else:
-            # dump as html
-            title_row = '<tr>%s</tr>' % ''.join(
-                ['<th>%s</th>' % title for title in self.titles()])
-            body_rows = ''
-            for _, row_list in release_to_activity_list.items():
-                body_rows += '<tr>%s</tr>' % ''.join(
-                    ['<td>%s</td>' % item for item in row_list])
-
-            print('<html><body><table border="1">%s%s</table></body></html>'
-                  % (title_row, body_rows))
-
+    @property
     def titles(self):
         """
         :return: a title row list which is like as
             ['release', 'user-id1', 'user-id2'....]
         """
-        title_row = ['release']
-        title_row.extend(self.user_id_list)
-        return title_row
+        titles = ['release']
+        titles.extend(self.user_id_list)
+        return titles
+
+
+def dump_result(result, titles, dump_format='html'):
+    """
+    :param result: a result dict that maps reslease -> user_id -> value
+    :param titles: a list of title row items. Note that titles[1:] will
+        be used to find items to dump
+    :param dump_format: html or csv
+    """
+    if dump_format not in ('html', 'csv'):
+        raise ValueError('dump_format is supported only for html or csv')
+
+    # release_to_activity_list is a dict to map a release to user
+    # activities (perhaps, we don't have to keep the release for the key
+    # because each row keep the release name in the 1st column)
+    release_to_activity_list = dict()
+    keys = titles[1:]
+
+    for release, result_info in result.items():
+        row_list = [release]
+        for key in keys:
+            # N.B. the result set may be already filtered when
+            # collecting the result but for safety, use .get and set
+            # 0 if the key not found in the dict)
+            row_list.append(str(result_info.get(key, 0)))
+        release_to_activity_list[release] = row_list
+
+    if dump_format == 'csv':
+        # dump as csv
+        print(','.join(titles))
+        for _, row_list in release_to_activity_list.items():
+            print(','.join(row_list))
+    else:
+        # dump as html
+        title_row = '<tr>%s</tr>' % ''.join(
+            ['<th>%s</th>' % title for title in titles])
+        body_rows = ''
+        for _, row_list in release_to_activity_list.items():
+            body_rows += '<tr>%s</tr>' % ''.join(
+                ['<td>%s</td>' % item for item in row_list])
+
+        print('<html><body><table border="1">%s%s</table></body></html>'
+              % (title_row, body_rows))
 
 
 def commit_count(commit_info_dict):
@@ -156,34 +162,66 @@ def lines_of_code(commit_info_dict):
     return lines_of_code
 
 
-class ReviewCollector(object):
-    def collect_stats(self, release):
-        filter_params = {'release': release}
-        filter_params.update(self.base_filter_params)
-        resp = requests.get(
-            os.path.join(base_request_url, self.url_tail), filter_params)
-        stats_list = json.loads(resp.content)['stats']
-        return stats_list
+def review_counts(stat_info_dict):
+    """
+    helper fucntion to summarize all from raw stat info from ReviewCollector
+    :param stat_info_dict: a raw infor dict that collect_stat_info
+        returns
+    :return: summary info that maps release -> identifier
+        (e.g. user id, company_name) -> value
+    """
+    review_counts = {}
+    for release, stat_dict in stat_info_dict.items():
+        review_counts_per_release = {}
+        for key, stat in stat_dict.items():
+            # review count
+            review_counts_per_release[key] = stat['metric']
+        review_counts[release] = review_counts_per_release
+    return review_counts
 
-    def review_counts(self):
-        review_counts = {}
+
+class ReviewCollector(object):
+    def __init__(self):
+        self.url = os.path.join(BASE_URL, VERSION, 'stats', self.category)
+
+    def collect_stat_info(self, releases=None, params=None):
+        """
+        TODO perhaps, we could make a BaseCollector for collecting activities
+        :param release: release name of openstack to collect, "all" can be
+            allowed to collect all activities
+        :param params: parameter dict
+        """
+        releases = releases or ['all']
+        params = params or {}
+        release_to_stat = {}
         for release in releases:
-            stats_per_release = self.collect_stats(release)
-            review_counts_per_release = {}
-            for stat in stats_per_release:
-                if stat['name'] not in self.target_names:
-                    continue
-                # review count
-                review_counts_per_release[stat['name']] = stat['metric']
-            review_counts[release] = review_counts_per_release
-        return review_counts
+            filter_params = {'release': release}
+            filter_params.update(params)
+            resp = requests.get(self.url, filter_params)
+            stats_list = json.loads(resp.content)['stats']
+            # filter stats only in targets
+            # N.B. it could be worth to make this as query
+            stats_list = dict(
+                [(stat['name'], stat) for stat in stats_list
+                 if stat['name'] in self.targets])
+            release_to_stat[release] = stats_list
+        return release_to_stat
+
+    @property
+    def titles(self):
+        """
+        :return: a title row list which is like as
+            ['release', 'user-id1', 'user-id2'....]
+        """
+        titles = ['release']
+        titles.extend(self.targets)
+        return titles
 
 
 class EngineerReviewCollector(ReviewCollector):
     # dump review_counts per engineer
-    url_tail = 'engineers'
-    base_filter_params = {}
-    target_names = [
+    category = 'engineers'
+    targets = [
         'Kota Tsuyuzaki', 'Kazuhiro MIYAHARA', 'Samuel Merritt',
         'Clay Gerrard', 'Alistair Coles', 'John Dickinson', 'Pete Zaitcev',
         'Matthew Oliver', 'Greg Holt', 'Christian Schwede', 'Peter Portante',
@@ -194,30 +232,33 @@ class EngineerReviewCollector(ReviewCollector):
 
 class ModuleReviewCollector(ReviewCollector):
     # dump review_counts per module
-    url_tail = 'modules'
-    base_filter_params = {}
-    target_names = ['swift', 'nova', 'neutron', 'swift3', 'storlets']
+    category = 'modules'
+    targets = ['swift', 'nova', 'neutron', 'swift3', 'storlets']
 
 
 class CompanyReviewCollector(ReviewCollector):
     # dump review_counts per company
-    url_tail = 'companies'
-    base_filter_params = {'module': 'swift'}
-    target_names = [
+    category = 'companies'
+    targets = [
         'NTT', 'SwiftStack', 'Red Hat', 'HP', 'HPE', 'Rackspace', 'IBM']
+    # N.B. right now *params* is not used
+    params = {'module': 'swift'}
 
 
 if __name__ == '__main__':
-    # review_counts_collector = CompanyReviewCollector()
-    # review_counts_collector.dump_review_counts_in_html()
+    # reviews
+    review_collector = EngineerReviewCollector()
+    info = review_collector.collect_stat_info(['pike'])
+    result = review_counts(info)
+    dump_result(result, review_collector.titles)
 
-    commit_collector = CommitCollector()
-    info = commit_collector.collect_commit_info(['pike'])
+    # commit_collector = CommitCollector()
+    # info = commit_collector.collect_commit_info(['pike'])
 
     # commit
-    result = commit_count(info)
-    # commit_collector.dump_result(result)
+    # result = commit_count(info)
+    # dump_result(result, commit_collector.titles)
 
     # loc
-    result = lines_of_code(info)
-    commit_collector.dump_result(result)
+    # result = lines_of_code(info)
+    # dump_result(result, commit_collector.titles)
